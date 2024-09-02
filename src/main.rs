76219d7959baf1eef740951bf0a1aeed989ccc82
Sam Uherek <samuherekbiz@gmail.com>
2024-09-02 21:17:19 +0200

    add word config from command line

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use rand::Rng;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
+    /// Minimum allowed characters. Default 3
     #[arg(long)]
     min: Option<u8>,
-    /// Optional name to operate on
+    /// Maximum allowed characters. Default 6
     #[arg(long)]
     max: Option<u8>,
-    ///
+    /// How many words to generate. Default 10
     #[arg(short, long)]
     count: Option<usize>,
-    // /// Turn debugging information on
-    // #[arg(short, long, action = clap::ArgAction::Count)]
-    // debug: u8,
 }
 
 const VOW: &str = "aeiou";
 const CON: &str = "bcdfghjklmnpqrstvwxyz";
+const PATTERNS: &[&str] = &[
+    "CCV", "CVC", "VCV", "VVC", "CVV", "VCC", "CVCV", "VCVC", "CVCC", "VCCV", "CVVC", "CCVV",
+];
 
-// <con><vow>
-// <con><vow><con>
-// <vow><con><vow>
-fn gen_word(rng: &mut rand::rngs::ThreadRng, min: usize, max: usize) -> String {
-    let start = rng.gen_range(0..=1);
-    let len = rng.gen_range(min..=max);
+fn gen_word(rng: &mut rand::rngs::ThreadRng, config: &WordConfig) -> String {
+    let first_vow = rng.gen_range(0..=1);
+    let len = rng.gen_range(config.min..=config.max);
     let mut buf = String::new();
 
-    for i in 0..len {
-        if i % 2 == start {
+    while buf.len() < len {
+        let c = if buf.len() % 2 == first_vow {
             let idx = rng.gen_range(0..VOW.len());
-            let c = VOW.chars().nth(idx).expect("To get the char");
-            buf.push(c);
+            VOW.chars().nth(idx).expect("To get the char")
         } else {
             let idx = rng.gen_range(0..CON.len());
-            let c = CON.chars().nth(idx).expect("To get the char");
+            CON.chars().nth(idx).expect("To get the char")
+        };
+
+        buf.push(c);
+
+        if config.with_double && rng.gen_bool(0.05) {
             buf.push(c);
         }
     }
@@ -44,15 +45,49 @@ fn gen_word(rng: &mut rand::rngs::ThreadRng, min: usize, max: usize) -> String {
     buf
 }
 
+struct WordConfig {
+    min: usize,
+    max: usize,
+    with_double: bool,
+}
+
+impl WordConfig {
+    fn new() -> Self {
+        WordConfig {
+            min: 3,
+            max: 6,
+            with_double: true,
+        }
+    }
+
+    fn with_min(&mut self, min: usize) -> &mut Self {
+        self.min = min;
+        self
+    }
+
+    fn with_max(&mut self, max: usize) -> &mut Self {
+        self.max = max;
+        self
+    }
+
+    fn with_double(&mut self, with_double: bool) -> &mut Self {
+        self.with_double = with_double;
+        self
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
-    let min = cli.min.unwrap_or(3);
-    let max = cli.max.unwrap_or(6);
-    let len = cli.count.unwrap_or(10);
+    let count = cli.count.unwrap_or(10);
     let mut rng = rand::thread_rng();
+    let mut config = WordConfig::new();
+    config
+        .with_min(cli.min.unwrap_or(3).into())
+        .with_max(cli.max.unwrap_or(6).into())
+        .with_double(true);
 
-    for _ in 0..len {
-        let word = gen_word(&mut rng, min.into(), max.into());
+    for _ in 0..count {
+        let word = gen_word(&mut rng, &config);
         println!("{}", word);
     }
 }
